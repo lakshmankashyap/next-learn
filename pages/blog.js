95202f45bc4c255685c00a9371ac39f7a28243e0
lakshmankashyap <lkashyap92@gmail.com>
2020-07-11 23:57:51 +0530

    first commit

---
+++ pages/blog.js
@@ -0,0 +1,7 @@
+const Blog = ()=>(
+    <div>
+        <h1>Blog page</h1>
+    </div>
+)
+
+export default Blog
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = ()=>(
+    <div>
+        <h1>Home Page</h1>
+    </div>
+)
+
+export default Index
