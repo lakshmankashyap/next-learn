c7b7708ac5dace34977f9a9f0ccf1c75b5dda7f5
lakshmankashyap <lkashyap92@gmail.com>
2020-07-13 12:37:21 +0530

    head and css add

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,22 @@
-const Blog = ()=>(
-    <div>
-        <h1>Blog page</h1>
-    </div>
+import Link from 'next/link'
+import posts from '../posts.json'
+
+const Blog = () => (
+  <div>
+    <h1>Blog</h1>
+
+    <ul>
+      {Object.entries(posts).map((value, index) => {
+        return (
+          <li key={index}>
+            <Link href='/blog/[id]' as={'/blog/' + value[0]}>
+              <a>{value[1].title}</a>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
 )
 
 export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Head from 'next/head';
+
 
 const Index = ()=>(
     <div>
+        <Head>
+            next js learn 
+        </Head>
         <h1>Home Page</h1>
         <Link href='/blog'>
             <a>Blog</a>
         </Link>
+        <style jsx>{`
+		  h1 {
+            font-size: 3rem;
+            color:red;
+		  }
+		`}</style>
     </div>
 )
 
